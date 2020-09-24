e7d641981d3ea431437faca52cff61da365e2713
= <awemulya@gmail.com>
2020-09-24 08:09:23 +0545

    do clean up

--- src/app/api/crud.py
+++ src/app/api/crud.py
@@ -3,7 +3,8 @@ from app.db import notes, database
 
 
 async def post(payload: NoteSchema):
-    query = notes.insert().values(title=payload.title, description=payload.description)
+    query = notes.insert().values(title=payload.title,
+                                  description=payload.description)
     return await database.execute(query=query)
 
 
